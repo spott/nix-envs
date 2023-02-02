d23c2bee603570a0f8578d3b8c4d87407d482caf
Andrew Spott <andrew.spott@gmail.com>
2023-02-02 09:14:30 -0700

    added a python aarch environment with python 3.11

---
+++ aarch64/python/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "aarch64-darwin";
+    overlays = [
+        (self: super: {
+          python = super.python311;
+        })
+      ];
+    pkgs = import nixpkgs { inherit system overlays; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ python ] ++ 
+          (with pkgs.python311Packages; [ pipenv ]);
+      };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   outputs = { self }: {
 
     templates = {
+      python-aarch64 = {
+        path = ./aarch64/python;
+        description = "python template using python 3.11 and pipenv for aarch64-darwin";
+      };
 
       pytorch-aarch64 = {
         path = ./aarch64/pytorch;
-        description = "pytorch template using pipenv for aarch64-darwin";
+        description = "pytorch template using python 3.10 and pipenv for aarch64-darwin";
       };
       
       pytorch-x86_64 = {
         path = ./x86_64/pytorch;
-        description = "pytorch template using pipenv for x86_64";
+        description = "pytorch template using python 3.10 and pipenv for x86_64";
       };
 
     };
