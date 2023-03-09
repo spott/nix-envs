3512ff953b6ddc0ae87d6600037fd1f2d22aa109
Andrew Spott <andrew.spott@gmail.com>
2023-03-09 12:07:48 -0700

    fixed generic flake to include unstable passthrough and to actually work

--- all/generic/flake.nix
+++ all/generic/flake.nix
@@ -2,15 +2,24 @@
   description = "Generic flake to add some programs to the environment";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
     with flake-utils.lib; eachSystem allSystems (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs-unstable = import nixpkgs-unstable { inherit system; };
+      overlays = [ (self: super: {
+                      #if you want to use specific packages from unstable rather than 22.11
+                      #<package> = pkgs-unstable.<package>;
+                      })
+                 ];
+      pkgs = import nixpkgs { inherit system overlays; };
     in {
-    devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ <insert packages here> ];
+    devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ 
+          #<insert packages here> 
+        ];
       };
   });
 }
